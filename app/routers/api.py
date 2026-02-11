a5935de7693dfc7bd684443a54735d7f5042a60c
Davide Di Caprio <davidedicaprio@Davides-MacBook-Air.local>
2026-02-11 18:22:09 +0100

    remove unused class

--- app/routers/api.py
+++
@@ -1,8 +0,0 @@
-
-from fastapi import APIRouter
-from app.routers import gas_price, car_info
-
-router = APIRouter()
-
-router.include_router(gas_price.router, tags=["Gas Price"])
-router.include_router(car_info.router)
